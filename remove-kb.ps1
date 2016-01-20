4850f846e73473c70f4071a29ff119127be0f3cc
Yorick <ephase@xieme-art.org>
2016-01-20 14:07:01 +0100

    Bug : update always show not installed

--- remove-kb.ps1
+++ remove-kb.ps1
@@ -43,15 +43,15 @@ function hide_update() {
 Foreach($kbID in $kbIDs){
     $kbNum = $kbID.Replace("KB","")
     Write-Host -NoNewline -ForegroundColor white "Uninstalling $kbID : "
-    
-    if ($exist){
-        Write-Host -NoNewline -ForegroundColor DarkGreen "found!" -ForegroundColor white "removing ... "
+    if (Get-HotFix -Id $kbID -ErrorAction SilentlyContinue)){
+        Write-Host -NoNewline -ForegroundColor DarkGreen "found!" 
+        Write-Host -Nonewline -ForegroundColor white "removing ... "
         wusa.exe /uninstall /KB:$kbNum  /norestart /quiet /log:wsua.log
         Do
 	    {
     		Start-Sleep -Seconds 3
     	}while(Invoke-Command -ScriptBlock {Get-Process  | Where-Object {$_.name -eq "wusa"}})
-        if(Get-HotFix -Id $kbID -ErrorAction SilentlyContinue){		
+        if(Get-HotFix -Id $kbID -ErrorAction SilentlyContinue){
 		    Write-Host -NoNewline -ForegroundColor Red "Failed"
 	    }
 	    else{
